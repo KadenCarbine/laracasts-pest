d5871d25a8b56204d8fb1d5fb644091618b5180f
Kaden Carbine <kadenjcar@gmail.com>
2025-09-24 16:10:47 -0400

    Episode 5

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+/** @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course> */
+class CourseFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
---
+++ tests/Feature/PagesResponseTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use function Pest\Laravel\get;
+
+it('returns a successful response', function () {
+    get(route('home'))
+        ->assertOk();
+});
